5b39db2b94d2ddcbcae15b33206c86df0015abed
Radprzem <114361982+Radprzem@users.noreply.github.com>
2023-11-29 10:08:40 +0100

    Create hello-traveller.is-an.app.js

---
+++ domains/hello-traveller.is-an.app.js
@@ -0,0 +1,33 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: 'I made this domain for a test.', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'hello-traveller', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/Radprzem/huh-fish',
+    email: 'dicky_poo_poou1234@yahoo.com',
+  },
+  record: {
+    CNAME: 'https://radprzem.github.io', // e.g.: <your-github-account>.github.io
+    TXT: ['list', 'of', 'required', 'txt', 'records'],
+    A: ['list', 'of', 'IPv4', 'addresses', 'like', 'a', '127.0.0.1'],
+    AAAA: ['list', 'of', 'IPv6', 'addresses', 'like', 'a', '::1'],
+    NS: ['list', 'of', 'nameservers'],
+  },
+  //proxy: false, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
+                  // should provide valid a SSL certificate and protection CF will be disabled
+  // nested: [{ // in addition, you may define the required nested subdomains
+  //   subdomain: 'foo',
+  //   record: {
+  //     CNAME: '...',
+  //   },
+  //   proxy: false,
+  // }, {
+  //   subdomain: 'bar',
+  //   record: {
+  //     A: ['...'],
+  //   },
+  //   proxy: true,
+  // }]
+})
